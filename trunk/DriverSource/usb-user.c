6c5af57527b7ff1349160b5a288666a65c6a804a
grzesiek.rzaca@gmail.com <grzesiek.rzaca@gmail.com@e374649b-d319-d8e9-3579-9ce5c70c9a1a>
2010-07-01 18:26:50 +0000

    Przepisałem usb-user.c tak aby mozna bylo wstrzymywac probkowanie i zmieniac parametry caluy czas mozna tez z linni komend odpalic dla skryptow

--- trunk/DriverSource/usb-user.c
+++ trunk/DriverSource/usb-user.c
@@ -33,6 +33,7 @@ int getch()
 
 
 
+
 void set_beginning(char *bufor, int pid)
 {
         ((int *)bufor)[0]=USB_AD_IOCTL_MAGIC_NUMBER;  
@@ -46,6 +47,80 @@ void set_beginning(char *bufor, int pid)
         ((int *)bufor)[8]=0;
         ((int *)bufor)[9]=0;
         ((int *)bufor)[10]=0;
+}
+
+
+void params(char *buffer,int pid){
+        char str[32];
+        int i;
+        printf("Podaj parametry probkowania\nPodaj czestotliwosc:\n");
+        set_beginning(buffer, pid);
+        scanf("%d",&((unsigned int *)buffer)[2]);
+        //((unsigned int*)buffer)[2] = 1638;
+        printf("Podaj ilosc probek zwracanych przez sterownik:\n");
+        scanf("%d",&((unsigned int *)buffer)[3]);
+        //((int*)buffer)[3] = 10;
+        printf("Podaj numery kanalow (1-7) z ktorych bedza wysylane probki:\n");
+        scanf("%s",str);
+        for (i = 0; i < 32; i++){
+                if(str[i]=='1'){
+                        ((int *)buffer)[4] = 1;
+                        }
+                if(str[i]=='2'){
+                        ((int *)buffer)[5] = 1;
+                        }
+                if(str[i]=='3'){
+                        ((int *)buffer)[6] = 1;
+                        }
+                if(str[i]=='4'){
+                        ((int *)buffer)[7] = 1;
+                        }
+                if(str[i]=='5'){
+                        ((int *)buffer)[8] = 1;
+                        }
+                if(str[i]=='6'){
+                        ((int *)buffer)[9] = 1;
+                        }
+                if(str[i]=='7'){
+                        ((int *)buffer)[10] = 1;
+                        }
+                }
+}
+
+int param_send(char *buffer, int usb_ad){
+        int ret_val;
+        printf("Staram sie wyslac IOCTLa z parametrami fq: %d size: %d channels: %d %d %d %d %d %d %d\n",
+        ((int *)buffer)[2],
+        ((int *)buffer)[3],
+        ((int *)buffer)[4],   
+        ((int *)buffer)[5],  
+        ((int *)buffer)[6],
+        ((int *)buffer)[7],  
+        ((int *)buffer)[8],  
+        ((int *)buffer)[9], 
+        ((int *)buffer)[10] 
+        );
+        ret_val = ioctl(usb_ad, IOCTL_SET_PARAMS, buffer);
+        printf("Odpowiedx: %d\n",ret_val);
+        return ret_val;
+}
+
+int get_data(char *buffer, int pid, int usb_ad, int block_size, int channel_count){
+        int i,j,ret_val;
+        set_beginning(buffer, pid);
+        ret_val = ioctl(usb_ad, IOCTL_GET_DATA, buffer);
+        
+        for (i = 0; i < block_size * channel_count * 2 - 1; i+=2) {
+                printf("%d,%d  ",
+                        buffer[i],
+                        buffer[i+1]);
+                j++;
+                if(j == channel_count){
+                        printf("\n");
+                        j = 0;
+                        }
+                }
+        return 0;
 }
 
 int main(int argc, char **argv) { 
@@ -78,101 +153,54 @@ int main(int argc, char **argv) {
                                 }
                         else{
                                 ((int *)buffer)[i+1] = 0;
-                                //printf("%d ",atoi(argv[i]));
                                 }
-        //((int *)buffer)[4]=argc[3];   
-        //((int *)buffer)[5]=argc[4];  
-        //((int *)buffer)[6]=argc[5];
-        //((int *)buffer)[7]=argc[6];  
-        //((int *)buffer)[8]=argc[7];  
-        //((int *)buffer)[9]=argc[8];  
-        //((int *)buffer)[10]=argc[9];  
                 }
         else{
-                printf("Podaj parametry probkowania\nPodaj czestotliwosc:\n");
-                set_beginning(buffer, pid);
-                //scanf("%d",&((int *)buffer)[2]);
-                ((unsigned int*)buffer)[2] = 1638;
-                printf("Podaj ilosc probek zwracanych przez sterownik:\n");
-                //scanf("%d",&((int *)buffer)[3]);
-                ((int*)buffer)[3] = 10;
-                printf("Podaj numery kanalow (1-7) z ktorych bedza wysylane probki:\n");
-                scanf("%s",str);
-                for (i = 0; i < 32; i++){
-                        if(str[i]=='1'){
-                                ((int *)buffer)[4] = 1;
-                                //printf("DBG bede probkowac kanal 1\n");
-                                }
-                        if(str[i]=='2'){
-                                ((int *)buffer)[5] = 1;
-                                //printf("DBG bede probkowac kanal 2\n");
-                                }
-                        if(str[i]=='3'){
-                                ((int *)buffer)[6] = 1;
-                                //printf("DBG bede probkowac kanal 3\n");
-                                }
-                        if(str[i]=='4'){
-                                ((int *)buffer)[7] = 1;
-                                //printf("DBG bede probkowac kanal 4\n");
-                                }
-                        if(str[i]=='5'){
-                                ((int *)buffer)[8] = 1;
-                                //printf("DBG bede probkowac kanal 5\n");
-                                }
-                        if(str[i]=='6'){
-                                ((int *)buffer)[9] = 1;
-                                //printf("DBG bede probkowac kanal 6\n");
-                                }
-                        if(str[i]=='7'){
-                                ((int *)buffer)[10] = 1;
-                                //printf("DBG bede probkowac kanal 7\n");
-                                }
-                        }
+                params(buffer, pid);
                 }
-
-        printf("Staram sie wyslac IOCTLa z parametrami fq: %d size: %d channels: %d %d %d %d %d %d %d\n",
-                ((int *)buffer)[2],
-                ((int *)buffer)[3],
-                ((int *)buffer)[4],   
-                ((int *)buffer)[5],  
-                ((int *)buffer)[6],
-                ((int *)buffer)[7],  
-                ((int *)buffer)[8],  
-                ((int *)buffer)[9], 
-                ((int *)buffer)[10] 
-                );
-        ret_val = ioctl(usb_ad, IOCTL_SET_PARAMS, buffer);
+        ret_val = param_send(buffer, usb_ad);
         printf("IOCTL odpowiedzial: %d\n", ret_val);
         //Liczymy z ilu kanalow zbieramy informacje (zeby sensowanie wyswietlac)
         for (i = 0; i < 7; i++)
                 if (((int *)buffer)[i+4] == 1)
                         channel_count++;
         block_size = ((int *)buffer)[3];
-        char c[10];
-
+        
         if (ret_val > -1){
                 printf("Wysyłam prosby o dane\n");
                 while((!kbhit())){
-                        set_beginning(buffer, pid);
-                        ret_val = ioctl(usb_ad, IOCTL_GET_DATA, buffer);
-                        printf("ioctl odpowiedx: %d debcount: %d\n", ret_val, debcount);
-                        printf("Zawartosc bufora [1bajt,2bajt] nacisnij enter aby przerwac probkowanie\n");
-                        for (i = 0; i < block_size * channel_count * 2 - 1; i+=2) {
-                                printf("%d,%d  ",
-                                        buffer[i],
-                                        buffer[i+1]);
-                                j++;
-                                if(j == channel_count){
-                                        printf("\n");
-                                        j = 0;
-                                        }
-                                }
-                        //debcount++;
+                        ret_val = get_data(buffer,pid,usb_ad,block_size,channel_count);
                         }
                 }
         //wyczyszczenie bufora zaznkow
         while(getch()!=10)
                 /*DONOTHING*/;
+        int loop = 1;
+        int dialog = 1;
+        while(loop == 1){
+                while(dialog == 1){
+                        printf("Co teraz?\n1-probkuj dalej\n2-podaj nowe parametry\n3-zakoncz prace programu\n");
+                        scanf("%d",((int *)str));
+                        if(str[0] == 1 || str[0] == 2 || str[0] == 3)
+                                dialog = 0;
+                        }
+                if (str[0] == 1){
+                        printf("Wysyłam prosby o dane\n");
+                        while((!kbhit()))
+                                ret_val = get_data(buffer,pid,usb_ad,block_size,channel_count);
+                        while(getch()!=10)/*DONOTHING*/;
+                        }
+                if (str[0] == 2){
+                        params(buffer,pid);
+                        param_send(buffer,pid);
+                        }
+                dialog = 1;
+                if (str[0] == 3){
+                        loop = 0;
+                        }
+                }
+
+        
         //sleep(2);
         close(usb_ad);
         printf("Urzadzenie zamkniete\n");
